d86bde238970d48e3f6f98515bf37af2f044413e
shn66 <42875310+shn66@users.noreply.github.com>
2018-12-11 23:48:25 -0800

    Update xref_interp.m

--- xref_interp.m
+++ xref_interp.m
@@ -25,18 +25,33 @@ function xref = xref_interp(x0,x1,v0,v1,dt,N)
 
 % [aT;bT;cT;dT] = [0 0 0 1; 1 1 1 1; 0 0 1 0; 3 2 1 0]\[f0T; f1T; df0T; df1T];
 % [a b c d] = [f0 f1 df0 df1]*inv([0 0 0 1; 1 1 1 1; 0 0 1 0; 3 2 1 0])'
+% %%
+% abcd = [x0 x1 v0 v1]*[  2   -3   0   1;
+%                        -2    3   0   0;
+%                         1   -2   1   0;
+%                         1   -1   0   0];                    
+% %%
+% T = 0:dt:N*dt;
+% 
+% % f = @(t) abcd*[t.^3; t.^2; t; 1];
+% 
+% % xref    = abcd*[T.^3; T.^2; T; ones(1,size(T,2))];
+% % xdotref = abcd*[3*T.^2; 2*T; ones(1,size(T,2)); zeros(1,size(T,2))];
+% 
+% xref = abcd*[T.^3 3*T.^2; T.^2 2*T; T ones(1,size(T,2)); ones(1,size(T,2)) zeros(1,size(T,2))]
+% 
+% %%
+% xref = reshape(xref',[10,11]); % 10: state vector dimension, 11: horizon
+T=[0 N*dt];
+Y=[v0 x0 x1 v1];
+T_seq=0:dt:N*dt;
+X_pos=spline(T,Y,T_seq);
+X_vel=zeros(size(X_pos,1),size(X_pos,2));
+X_vel(:,1)=v0;
 
-abcd = [x0 x1 v0 v1]*[  2   -3   0   1;
-                       -2    3   0   0;
-                        1   -2   1   0;
-                        1   -1   0   0];                    
-                    
-T = 0:dt:N*dt;
+xref=zeros(10,N+1); xref(:,1)=reshape([x0';v0'],[10 1]);
 
-% f = @(t) abcd*[t.^3; t.^2; t; 1];
-
-% xref    = abcd*[T.^3; T.^2; T; ones(1,size(T,2))];
-% xdotref = abcd*[3*T.^2; 2*T; ones(1,size(T,2)); zeros(1,size(T,2))];
-
-xref = abcd*[T.^3 3*T.^2; T.^2 2*T; T ones(1,size(T,2)); ones(1,size(T,2)) zeros(1,size(T,2))];
-xref = reshape(xref',[10,11]); % 10: state vector dimension, 11: horizon
+for i=1:N
+    X_vel(:,i+1)=1/dt*(X_pos(:,i+1)-X_pos(:,i));
+    xref(:,i+1)=reshape([X_pos(:,i+1)';X_vel(:,i+1)'],[10 1]);
+end
