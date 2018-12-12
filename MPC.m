95686dbc8a1f12802a705df861e0d3ca1bb16b3f
shn66 <42875310+shn66@users.noreply.github.com>
2018-12-11 23:46:38 -0800

    Update MPC.m

--- MPC.m
+++ MPC.m
@@ -1,26 +1,26 @@
- function X_wp = MPC(x0, Xref,xc, V_bar)
+function X_wp = MPC(x0, Xref,xc, V_bar,del_T,N)
  %% Simulation Parameters
- N=10;
- del_T=0.01;
+ 
  
  %% Cost Parameters
- Q=eye(10);R=2*eye(3);
+ Q=blkdiag(25*eye(8),0.5*eye(2));R=2*eye(3);
  P=10*Q;
- Xref=zeros(10,N+1);
+ %Xref=zeros(10,N+1);
  
  
  %% System Model Parameters
  
  % State Vector = [X Vx Pitch Pitch_Rate Y Vy Roll Roll_Rate Z Vz]^T
- bp0=1;
- bp1=1;
+ % GIVE VELOCITIES TO ROS: X
+ bp0=4;
+ bp1=4;
  g=9.8;
- br0=1;
- br1=1;
+ br0=4;
+ br1=4;
  A=blkdiag([0 1 0 0;0 0 g 0;0 0 0 1;0 0 -bp0 -bp1],[0 1 0 0;0 0 -g 0;0 0 0 1;0 0 -br0 -br1],[0 1;0 0]);
  ap=1;
  ar=1;
- m=1;
+ m=0.2;
  B=[zeros(3,3);-ap 0 0;zeros(3,3);0 -ar 0;0 0 0;0 0 1/m];
  G=[zeros(9,1);-g];
  % Computing Discrete time system matrices
@@ -31,9 +31,10 @@
  Temp=ss(A,G,[],[]);
  Temp=c2d(Temp,del_T);
  GT=Temp.B;
+ 
  %% Constraint Sets
  % Input Constraints
- uL=[-1/3*pi*ones(2,1);0];uU=[1/3*pi*ones(2,1);5];
+ uL=[-1/3*pi*ones(2,1);0];uU=[1/3*pi*ones(2,1);5*m*g];
  % State Constraints of the form A_ie*X<=b_ie
  A_ie=[0 0 1 0 0 0 0 0 0 0;
        0 0 -1 0 0 0 0 0 0 0;
@@ -80,9 +81,7 @@ Cost=(X(:,N+1)-Xref(:,N+1))'*P*(X(:,N+1)-Xref(:,N+1));
 for i=1:N
     Cost=Cost+(X(:,i)-Xref(:,i))'*Q*(X(:,i)-Xref(:,i))+U(:,i)'*R*U(:,i);
     Constraints=Constraints+[X(:,i+1)==AT*X(:,i)+BT*U(:,i)+GT];
-    Constraints=Constraints+[uL<=U(:,i)<=uU];
-    %     [A_f*X(:,i)<=b_f];
-    %     [A_ie*X(:,i)<=b_ie];
+    Constraints=Constraints+[uL<=U(:,i)<=uU]+[A_ie*X(:,i)<=b_ie];%+[A_f*X(:,i)<=b_f];
 end
 sol=optimize(Constraints, Cost);
 if sol.problem==1
